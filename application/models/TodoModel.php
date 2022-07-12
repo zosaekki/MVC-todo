0b02e2ec64aa86633fe8bb70070043a313a52a1b
zosaekki <101864441+zosaekki@users.noreply.github.com>
2022-07-12 17:28:22 +0900

    first commit

---
+++ application/models/TodoModel.php
@@ -0,0 +1,39 @@
+<?php
+    namespace application\models;
+    use PDO;
+
+    class TodoModel extends Model {
+        public function insTodo(&$param) {
+            $sql = "INSERT INTO t_todo
+                    (todo)
+                    VALUES
+                    (:todo)
+                    ";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->bindValue(":todo", $param["todo"]);
+            $stmt->execute();
+            return intval($this->pdo->lastInsertId());
+        }
+
+        public function selTodoList() {
+            $sql = "SELECT *
+                    FROM t_todo
+                    ";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_OBJ);
+        }
+
+        public function delTodo(&$param){
+            $sql = 
+            " DELETE FROM t_todo
+            ";
+            $itodo = $param["itodo"];
+            if($param["itodo"] > 0){
+              $sql .= " WHERE itodo = {$itodo}";
+            }
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute();
+            return $stmt->rowCount();
+          }
+    }
